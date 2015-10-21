871c4ba2775fbb6ea850c62e0b70b40c10ed95c5
P1p3 <pipeluck@gmail.com>
2015-10-20 22:23:00 -0500

    asdad

---
+++ Presentacion.Angular/controllers/sportsStore.js
@@ -0,0 +1,8 @@
+﻿angular.module("sportsStore", [])
+.controller("sportsStoreCtrl", function ($scope) {
+    $scope.data = {products:[{name:"Producto #1",description:"A product",category:"Category #1",price:100},
+                             {name:"Producto #2",description:"A product",category:"Category #1",price:110},
+                             {name:"Producto #3",description:"A product",category:"Category #2",price:210},
+                             {name:"Producto #4",description:"A product",category:"Category #3",price:202}]};
+
+});
