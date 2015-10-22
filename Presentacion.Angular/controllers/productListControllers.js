a541dec1e56c7026beaec5b5aba9bdb29efb67dd
P1p3 <pipeluck@gmail.com>
2015-10-21 21:56:51 -0500

    asdad

---
+++ Presentacion.Angular/controllers/productListControllers.js
@@ -0,0 +1,18 @@
+﻿angular.module("sportsStore")
+ .constant("productListActiveClass","btn-primary")
+.controller("productListController", function ($scope, $filter, productListActiveClass) {
+    var selectedCategory = null;
+
+    $scope.selectCategory = function (newCategory) {
+        debugger;
+        selectedCategory = newCategory;
+    }
+
+    $scope.categoryFilterFn = function (product) {
+        return selectedCategory == null || product.category == selectedCategory;
+    }
+
+    $scope.getCategoryClass = function (category) {
+        return selectedCategory == category ? productListActiveClass : "";
+    }
+});
